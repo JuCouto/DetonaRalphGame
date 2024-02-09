c2ac95342fbec1018e98d89d3e2e266e477381dc
Ju Couto <jnalmeida@ymail.com>
2024-02-09 12:31:37 -0300

    adicionando função de click

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -9,6 +9,8 @@ const state = {
   values: {
     timerId: null,
     gameVelocity:1000,
+    hitPosition: 0,//posição inicial
+    result:0,//
   },
 };
 
@@ -21,10 +23,10 @@ function randomSquare() {
   let randomNumber = Math.floor(Math.random() * 9); //6
   let randomSquare = state.view.squares[randomNumber]; //7
   randomSquare.classList.add("enemy"); //8
-  state.values.hitPosition = randomSquare.id;
+  state.values.hitPosition = randomSquare.id;// toda vez q eu sortear um quadrado aleátório,voi guardar o id do quadrado aleatório que eu sorteei.
 }
 
-//mover o inimigo
+//função para mover o inimigo
 function moveEnemy() {
     //vai guardar dentro de uma variavel temporária(timerId), chamo a função randomsquare, a cada x tempo ele vai chamar a função, passo o tempo que eu defini no gameVelocity.
   state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
@@ -33,23 +35,29 @@ function moveEnemy() {
 function addListenerHitBox() {
   //   //em state.view vai pegar as squares e definir uma função.
   state.view.squares.forEach(
-    (square) => {}
-    //     square.addEventListener("mousedown", () => {
-    //       //verificar se o id do elemento square é igual á posição do elemento selecionado.
-    //       if (square.id === state.values.hitPosition) {
-    //         state.values.result++;
-    //         state.view.score.textContent = state.values.result;
-    //         state.values.hitPosition = null;
+    (square) => {
+    //addEventListener função do javascript que fica escutando todo valor que é atibuido e chama uma função.
+        square.addEventListener("mousedown", () => {
+    //       //verificar se o id do elemento square(quadrado que o usuário clicou) é igual á posição do quadrado aleatório que foi soteado..
+          if (square.id === state.values.hitPosition) {
+            //se acertou o quadrado que o inimigo está, adiciono 1 a resultado.
+            state.values.result++;
+            //alterar o visual do score.
+            state.view.score.textContent = state.values.result;
+    //para o usuario não clicar no mesmo lugar seto para nulo.
+            state.values.hitPosition = null;
     //         playSound("hit");
-    //       }
-    //     });
-    //   }
+          }
+        });
+      }
   );
 }
 
 //função de início ou função main (init,initialize... oq importa é o conceito), função principal para chamar funções iniciais.
+//inicia o jogo movendo o inimigo e escutando os clicks.
 function initialize() {
   moveEnemy();
+  addListenerHitBox()
 }
 
 //executar oq está no escopo da função
