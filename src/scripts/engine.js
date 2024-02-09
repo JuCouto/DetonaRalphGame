60c9a45ec86444689063cfcfaf00773d0ed4fca0
Ju Couto <jnalmeida@ymail.com>
2024-02-09 17:52:49 -0300

    Adicionando sons

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -7,14 +7,20 @@ const state = {
     score: document.querySelector("#score"),
   },
   values: {
+    //values guardam valores e também coisas que estão acontecendo por baixo dos panos.
     timerId: null,
-    gameVelocity:1000,
-    hitPosition: 0,//posição inicial
-    result:0,//
+    gameVelocity: 1000,
+    hitPosition: 0, //posição inicial
+    result: 0, //score inicia em 0
+    currentTime: 60,
+  },
+  actions: {
+    //chamadas de funções, que executam ações.
+    countDownTimerId: setInterval(countDown, 1000), // 4
   },
 };
 
-//4
+//função para ter um quadrado aleatório para sortear o inimigo.
 function randomSquare() {
   //5
   state.view.squares.forEach((square) => {
@@ -23,41 +29,61 @@ function randomSquare() {
   let randomNumber = Math.floor(Math.random() * 9); //6
   let randomSquare = state.view.squares[randomNumber]; //7
   randomSquare.classList.add("enemy"); //8
-  state.values.hitPosition = randomSquare.id;// toda vez q eu sortear um quadrado aleátório,voi guardar o id do quadrado aleatório que eu sorteei.
+  state.values.hitPosition = randomSquare.id; //9
 }
 
 //função para mover o inimigo
 function moveEnemy() {
-    //vai guardar dentro de uma variavel temporária(timerId), chamo a função randomsquare, a cada x tempo ele vai chamar a função, passo o tempo que eu defini no gameVelocity.
-  state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
+  state.values.timerId = setInterval(randomSquare, state.values.gameVelocity); //10
 }
+
 //adicionar um evento de click do mouse dentro das caixinhas.
 function addListenerHitBox() {
-  //   //em state.view vai pegar as squares e definir uma função.
   state.view.squares.forEach(
+    //11
     (square) => {
-    //addEventListener função do javascript que fica escutando todo valor que é atibuido e chama uma função.
-        square.addEventListener("mousedown", () => {
-    //       //verificar se o id do elemento square(quadrado que o usuário clicou) é igual á posição do quadrado aleatório que foi soteado..
-          if (square.id === state.values.hitPosition) {
-            //se acertou o quadrado que o inimigo está, adiciono 1 a resultado.
-            state.values.result++;
-            //alterar o visual do score.
-            state.view.score.textContent = state.values.result;
-    //para o usuario não clicar no mesmo lugar seto para nulo.
-            state.values.hitPosition = null;
-    //         playSound("hit");
-          }
-        });
-      }
+      square.addEventListener("mousedown", () => {//12
+        
+        if (square.id === state.values.hitPosition) {//13
+          state.values.result++; //14
+          state.view.score.textContent = state.values.result; //15
+          state.values.hitPosition = null; //16
+          playSound("hit");
+        } else {
+          playSound("wrong");
+        }
+      });
+    }
   );
+}
+
+//
+function countDown() {
+ 
+  state.values.currentTime--; //17
+  state.view.timeLeft.textContent = state.values.currentTime; //18
+  
+  if (state.values.currentTime <= 0) {//19
+    clearInterval(state.actions.countDownTimerId);
+    clearInterval(state.actions.timerId);
+    alert("Game Over! O seu resultado foi: " + state.values.result);
+  }
+  if (state.values.currentTime == 1) {
+    playSound("gameover");
+  }
+}
+
+function playSound(audioName) {
+  let audio = new Audio(`./src/audios/${audioName}.m4a`);
+  audio.volume = 0.1;
+  audio.play();
 }
 
 //função de início ou função main (init,initialize... oq importa é o conceito), função principal para chamar funções iniciais.
 //inicia o jogo movendo o inimigo e escutando os clicks.
 function initialize() {
   moveEnemy();
-  addListenerHitBox()
+  addListenerHitBox();
 }
 
 //executar oq está no escopo da função
